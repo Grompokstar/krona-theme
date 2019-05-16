1d483ce9d6aadb31f5cbc4d98ab46c1e1ca8b67d
Roman Chervontsev <roma.chervontsev@gmail.com>
2019-05-16 18:27:58 +0300

    Отображение поста

--- index.php
+++ index.php
@@ -5,27 +5,27 @@ get_header();
 
     <section id="primary" class="content-area">
         <main id="main" class="site-main">
-
+          <div class="container">
             <?php
             if ( have_posts() ) {
 
-                // Load posts loop.
-                while ( have_posts() ) {
-                    the_post();
-                    get_template_part( 'template-parts/content/content' );
-                }
+              // Load posts loop.
+              while ( have_posts() ) {
+                the_post();
+                get_template_part( 'template-parts/content/content' );
+              }
 
-                // Previous/next page navigation.
-                //twentynineteen_the_posts_navigation();
+              // Previous/next page navigation.
+              //twentynineteen_the_posts_navigation();
 
             } else {
 
-                // If no content, include the "No posts found" template.
-                get_template_part( 'template-parts/content/content', 'none' );
+              // If no content, include the "No posts found" template.
+              get_template_part( 'template-parts/content/content', 'none' );
 
             }
             ?>
-
+          </div>
         </main><!-- .site-main -->
     </section><!-- .content-area -->
 
--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -18,6 +18,7 @@
 			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
 		}
 		if ( is_singular() ) :
+      the_date('d F Y', '<div class="post-date">', '</div>');
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
